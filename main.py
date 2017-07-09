4a9e97ae489dbeb65b62a899662227e5bbfbe56b
Angela Grammatas <angelagrammatas@Angelas-MacBook-Pro.local>
2017-07-09 09:14:43 +0200

    basic read & write!

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ app = Flask(__name__)
 def hello_world():
     # return 'Hello World!'
     # return render_template('test.html',number_of_results=num)
+
+    # ### to find out about using firebase data within python, https://firebase.google.com/docs/database/admin/retrieve-data
+    # print "ok!"
+    #
+    # # Import database module.
+    # from firebase_admin import db
+    #
+    # print "ok again!"
+    #
+    # # Get a database reference to our posts
+    # # ref = db.reference('server/saving-data/fireblog/posts')
+    # ref = db.reference('trust-2e8ba')
+    #
+    # # Read the data at the posts reference (this is a blocking operation)
+    # print(ref.get())
+
     return render_template('test.html')
 
 
